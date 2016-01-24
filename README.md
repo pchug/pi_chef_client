f9ad2924a7af5eb6fc72a32a1bad7736f78665e6
Piyush Chugh <piyush.chugh@gmail.com>
2016-01-24 08:21:11 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# pi_chef_client
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: pi_chef_client
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
