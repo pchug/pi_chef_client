4d369f0dca58c68275425d2cf20a4d8bb4b3e3e2
Piyush Chugh <piyush.chugh@gmail.com>
2016-01-24 08:24:48 -0500

    invoke chef-client::default

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures pi_chef_client'
 long_description 'Installs/Configures pi_chef_client'
-version '0.1.0'
+version '0.1.1'
 
 depends 'chef-client'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,1 @@
-#
-# Cookbook Name:: pi_chef_client
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+include_recipe 'chef-client::default'
